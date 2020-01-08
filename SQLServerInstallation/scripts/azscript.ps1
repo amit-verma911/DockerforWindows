73402de867d3812185af5d41bd42d67fbf6aac08
amit-verma911 <57206891+amit-verma911@users.noreply.github.com>
2020-01-08 23:15:20 +0530

    Update azscript.ps1

--- SQLServerInstallation/scripts/azscript.ps1
+++ SQLServerInstallation/scripts/azscript.ps1
@@ -1,5 +1,5 @@
 #Install AZ CLI
-Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi; Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet' -ErrorAction Stop;
+#Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi; Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet' -ErrorAction Stop;
 
 Az login --service-principal -u 47be0433-a943-4d33-aec0-a8e29fc16bd7 -p E8@D_Y9c5es11j?-KGPU4!04j5?x8_HD -t 05d75c05-fa1a-42e7-9cf1-eb416c396f2d -ErrorAction Stop;
 
